95735afa0671f21fdbd0fa54f9e169dd6013ae7e
kewegner <kewegner@us.ibm.com>
2019-06-26 10:54:04 -0500

    copyright changed

--- test/utils.go
+++ test/utils.go
@@ -1,6 +1,6 @@
 package test
 
-// Copyright © 2019 The Appsody Authors
+// Copyright © 2019 IBM Corporation and others.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
