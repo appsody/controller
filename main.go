364a892450ab3d208fce614374da947bd2e482a8
kewegner <47402596+kewegner@users.noreply.github.com>
2019-09-16 10:46:07 -0500

    Disable dir events (#26)
    * temporary key change
    
    * reverted key
    
    * do not watch for directory events
    
    * fix space

--- main.go
+++ main.go
@@ -440,8 +440,8 @@ func runWatcher(fileChangeCommand string, dirs []string, killServer bool) error 
 			select {
 			case event := <-w.Event:
 				ControllerDebug.log("File watch event detected for:  " + event.Path)
-				if unwatchDir(event.Path) {
-					ControllerDebug.log("The path ", event.Path, " is not to be watched")
+				if unwatchDir(event.Path) || event.IsDir() {
+					ControllerDebug.log("The path ", event.Path, " is not to be watched. Or this is a directory event which will not be watched.")
 				} else {
 					ControllerDebug.log("Determining if file or directory matches REGEX for:  " + event.Name())
 					if r.MatchString(event.Name()) {
