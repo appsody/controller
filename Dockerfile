6f44836649d1b789c97d0370217747bb24e77c68
kewegner <kewegner@us.ibm.com>
2019-11-07 15:18:35 -0600

    change image name and remove script file

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM busybox
-COPY ./setController.sh /setController.sh
+
 COPY ./package/appsody-controller /appsody-controller
-RUN chmod +x /setController.sh /appsody-controller
+RUN  chmod +x /appsody-controller
 WORKDIR /
--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@
 set -e
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build -t $DOCKER_ORG/install-controller:$TRAVIS_TAG -t $DOCKER_ORG/install-controller:latest .
-docker push $DOCKER_ORG/appsody-controller
+docker push $DOCKER_ORG/install-controller
