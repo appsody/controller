5129d50dcbad972d91c4de8bd8b7023a19a231a8
Cameron McWilliam <cam.mcwilliam@me.com>
2020-02-21 09:12:16 +0000

    have to use buildx

--- build.sh
+++ build.sh
@@ -3,5 +3,4 @@
 
 set -e
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build -t $DOCKER_ORG/init-controller:$TRAVIS_TAG -t $DOCKER_ORG/init-controller:latest .
-docker push $DOCKER_ORG/init-controller
+docker buildx build -t $DOCKER_ORG/init-controller:$TRAVIS_TAG -t $DOCKER_ORG/init-controller:latest --platform=linux/amd64,linux/ppc64le . --push
