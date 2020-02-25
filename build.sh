aef326707edc79e07ab5f14ffef8b817385fbeb8
Cameron McWilliam <cam.mcwilliam@me.com>
2020-02-25 08:58:40 +0000

    trying travis

--- build.sh
+++ build.sh
@@ -3,4 +3,7 @@
 
 set -e
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+export DOCKER_CLI_EXPERIMENTAL=enabled
+docker buildx create --name mybuilder
+docker buildx use mybuilder
 docker buildx build -t $DOCKER_ORG/init-controller:$TRAVIS_TAG -t $DOCKER_ORG/init-controller:latest --platform=linux/amd64,linux/ppc64le . --push
