dce1c3b8857a8bdea2be80f207d8a391f117d77a
Cameron McWilliam <cam.mcwilliam@me.com>
2020-03-16 08:20:02 +0000

    check contnets of package

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@
 set -e
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 export DOCKER_CLI_EXPERIMENTAL=enabled
+ls -la ./package/appsody-controller-linux/
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker buildx create --name mybuilder
 docker buildx use mybuilder
