f389875766d2dd981095d4f7573aa53c6ac35a2c
Cameron McWilliam <cam.mcwilliam@me.com>
2020-03-16 08:29:30 +0000

    list contents

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@
 set -e
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 export DOCKER_CLI_EXPERIMENTAL=enabled
-ls -la ./package/appsody-controller-linux/
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+ls -la
 docker buildx create --name mybuilder
 docker buildx use mybuilder
 docker buildx build -t $DOCKER_ORG/init-controller:$TRAVIS_TAG -t $DOCKER_ORG/init-controller:latest --platform=linux/amd64,linux/ppc64le,linux/s390x . --push
