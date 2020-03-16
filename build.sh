1da4de0a394ba87bc879413d44bffe27d38cd5d6
Cameron McWilliam <cam.mcwilliam@me.com>
2020-03-16 08:33:38 +0000

    list package

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -e
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 export DOCKER_CLI_EXPERIMENTAL=enabled
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-ls -la
+ls -la ./package/
 docker buildx create --name mybuilder
 docker buildx use mybuilder
 docker buildx build -t $DOCKER_ORG/init-controller:$TRAVIS_TAG -t $DOCKER_ORG/init-controller:latest --platform=linux/amd64,linux/ppc64le,linux/s390x . --push
