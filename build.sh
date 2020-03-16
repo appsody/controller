ad56440e6b5f0aac9d36a93ee4711e59cd97daf6
Cameron McWilliam <cam.mcwilliam@me.com>
2020-03-16 08:51:13 +0000

    reverted back to allow merge into appsody

--- build.sh
+++ build.sh
@@ -6,7 +6,6 @@ set -e
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 export DOCKER_CLI_EXPERIMENTAL=enabled
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-ls -la ./package/
 docker buildx create --name mybuilder
 docker buildx use mybuilder
-docker buildx build -t $DOCKER_ORG/init-controller:$TRAVIS_TAG -t $DOCKER_ORG/init-controller:latest --platform=linux/amd64,linux/ppc64le,linux/s390x . --push
+docker buildx build -t appsody/init-controller:$TRAVIS_TAG -t appsody/init-controller:latest --platform=linux/amd64,linux/ppc64le,linux/s390x . --push
