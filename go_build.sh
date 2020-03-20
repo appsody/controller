e005cd1551ff8b02bf4cd778434e4b71183bb3de
Cameron McWilliam <cam.mcwilliam@me.com>
2020-03-20 08:01:22 +0000

    fixed declare

--- go_build.sh
+++ go_build.sh
@@ -2,7 +2,7 @@
 set -e
 for OS_ARCH in linux/amd64 linux/ppc64le linux/s390x
 do
-   declare OS $(echo $OS_ARCH | cut -f1 -d/)
-   declare ARCH $(echo $OS_ARCH | cut -f2 -d/)
+   declare OS=$(echo $OS_ARCH | cut -f1 -d/)
+   declare ARCH=$(echo $OS_ARCH | cut -f2 -d/)
    GOOS="$OS" CGO_ENABLED=0 GOARCH="$ARCH" go build -o ./build/appsody-controller-"$OS_ARCH" -ldflags "-X main.VERSION=$1"
 done
